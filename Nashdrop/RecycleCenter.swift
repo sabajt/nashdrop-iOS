eb4b78d6d1fb42a7b4628617bd6b7f5545eb3cfb
“pkadams67” <paul@pkadams67.io>
2016-06-04 12:42:29 -0500

    Renamed object Strings to match API's

--- Nashdrop/RecycleCenter.swift
+++ Nashdrop/RecycleCenter.swift
@@ -10,30 +10,30 @@ import Foundation
 
 class RecycleCenter {
 
-    static let kNameKey = "name"
-    static let kPhoneKey = "phone"
-    static let kStreetKey = "street"
-    static let kCityKey = "city"
-    static let kStateKey = "state"
-    static let kZIPKey = "zip"
+    static let kNameKey = "facility_name"
+    static let kAddressKey = "mapped_location_address"
+    static let kCityKey = "mapped_location_city"
+    static let kStateKey = "mapped_location_state"
+    static let kZIPKey = "mapped_location_zip"
     static let kAcceptedItemsKey = "acceptedItems"
+    static let kPhoneKey = "phone"
     static let kResultsKey = "results"
 
     var name: String?
-    var phone: String?
-    var zip: String?
-    var street: String?
+    var address: String?
     var city: String?
     var state: String?
+    var zip: String?
+    var phone: String?
     var acceptedItems: String?
 
     init(jsonDictionary: [String: String]) {
         self.name = jsonDictionary[RecycleCenter.kNameKey]
-        self.phone = jsonDictionary[RecycleCenter.kPhoneKey]
-        self.street = jsonDictionary[RecycleCenter.kStreetKey]
+        self.address = jsonDictionary[RecycleCenter.kAddressKey]
         self.city = jsonDictionary[RecycleCenter.kCityKey]
         self.state = jsonDictionary[RecycleCenter.kStateKey]
         self.zip = jsonDictionary[RecycleCenter.kZIPKey]
-        self.acceptedItems = jsonDictionary[RecycleCenter.kAcceptedItemsKey]
+        self.phone = jsonDictionary[RecycleCenter.kPhoneKey]
+//        self.acceptedItems = jsonDictionary[RecycleCenter.kAcceptedItemsKey]
     }
 }
